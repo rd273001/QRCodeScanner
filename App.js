4392bc84307b660905694a77c477d07283ba94d0
rd273001 <rd273001@gmail.com>
2024-02-22 13:55:31 +0530

    Initital App created

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+
+  return (
+    <Text>
+      QR Code Scanner App
+    </Text>
+  );
+};
+
+export default App;
